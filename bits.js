f736453e5077e02a8845ebd5ed7cf33ff7e7c0c2
chuxiaonan <chuxiaonan@gmail.com>
2015-06-11 20:39:01 +0800

    Creat bits.js

---
+++ bits.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Baidu Index Transcribe Style Script
+// @namespace    https://github.com/chuxiaonan
+// @version      0.1
+// @description  Inverse Baidu Index's Style
+// @author       Chu Xiaonan
+// @match        https://www.baidu.com/
+// @grant        none
+// ==/UserScript==
+
+function add_css(str_css) {
+     try {
+          var style = document.createStyleSheet();
+          style.cssText = str_css;
+     }
+     catch (e) {
+          var style = document.createElement("style");
+          style.type = "text/css";
+          style.textContent = str_css;
+          document.getElementsByTagName("head")[0].appendChild(style);
+	}
+}
+
+window.onload = function() {
+    var s1 = '.s-skin-hasbg .s-nplus .s-ncf-name .s-ncf-name-link, .s-skin-hasbg .s-nplus .s-nav-name em, .s-skin-hasbg .s-btn-backto-nused span, .s-skin-hasbg .s-nori .s-nori-nav em, .s-skin-hasbg .s-nav-edit-addform .addform-inner-cbx-container a, .s-skin-hasbg .s-nav-edit-addform .addform-add-btn {color:#ffffff}';
+    var s2 = '#s_content_1 .s-nall-tabs a {color:#ffffff}';
+    var s3 = '.s-nav-opts .s-nav-opt {color:#ffffff}';
+	add_css(s3+s1+s2);
+};
+
+/* End of file bits.js */
+/* Location: ./bits.js */
