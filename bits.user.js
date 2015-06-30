b4402a733d3e0ec70b243d5831a96f9fe433845a
Chu Xiaonan <chuxiaonan@gmail.com>
2015-06-30 14:05:31 +0800

    Update bits.user.js

--- bits.user.js
+++ bits.user.js
@@ -12,11 +12,11 @@
 // ==/UserScript==
 
 function add_css(str_css) {
-    try {
+    try { //firefox
         var style = document.createStyleSheet();
         style.cssText = str_css;
     }
-    catch (e) {
+    catch (e) { //chrome
         var style = document.createElement("style");
         style.type = "text/css";
         style.textContent = str_css;
@@ -28,7 +28,7 @@ window.onload = function() {
     var s1 = '.s-skin-hasbg .s-nplus .s-ncf-name .s-ncf-name-link, .s-skin-hasbg .s-nplus .s-nav-name em, .s-skin-hasbg .s-btn-backto-nused span, .s-skin-hasbg .s-nori .s-nori-nav em, .s-skin-hasbg .s-nav-edit-addform .addform-inner-cbx-container a, .s-skin-hasbg .s-nav-edit-addform .addform-add-btn {color:#ffffff}';
     var s2 = '#s_content_1 .s-nall-tabs a {color:#ffffff}';
     var s3 = '.s-nav-opts .s-nav-opt {color:#ffffff}';
-	add_css(s3+s1+s2);
+    add_css(s3+s1+s2);
 };
 
 /* End of file bits.user.js */
