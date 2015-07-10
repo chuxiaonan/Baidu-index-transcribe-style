1ef46a866327c94190d7f1f42ad69dc18c301d8d
Chu Xiaonan <chuxiaonan@gmail.com>
2015-07-11 03:13:52 +0800

    Update bits.user.js
    1.Add inverse style of the "Add website" panel

--- bits.user.js
+++ bits.user.js
@@ -29,7 +29,9 @@ window.onload = function() {
     var s1 = '.s-skin-hasbg .s-nplus .s-ncf-name .s-ncf-name-link, .s-skin-hasbg .s-nplus .s-nav-name em, .s-skin-hasbg .s-btn-backto-nused span, .s-skin-hasbg .s-nori .s-nori-nav em, .s-skin-hasbg .s-nav-edit-addform .addform-inner-cbx-container a, .s-skin-hasbg .s-nav-edit-addform .addform-add-btn {color:#ffffff}';
     var s2 = '#s_content_1 .s-nall-tabs a {color:#ffffff}';
     var s3 = '.s-nav-opts .s-nav-opt {color:#ffffff}';
-    add_css(s3+s1+s2);
+    var s4 = '.s-skin-hasbg .s-nadd .s-nadd-recommend .s-nadd-dir .s-dir-title, .s-skin-hasbg .s-nadd .s-nadd-recommend .s-nadd-dir .s-nadd-nav, .s-skin-hasbg .s-nform-plus .s-select-input-mask {color:#ffffff}';
+    var s5 = '.s-skin-hasbg .s-nadd .s-nadd-form .cite-name-input, .s-skin-hasbg .s-nadd .s-nadd-form .cite-url-input, .s-skin-hasbg .s-nadd-form .addform-input-area .addform-input, .s-skin-hasbg .s-nav-edit-addform .addform-input-area .addform-input {color:#ffffff}';
+    add_css(s3+s1+s2+s4+s5);
     //remove "More Productions" on right above of the page
     var a_s_bri = $('.s_bri');
     a_s_bri.remove();
